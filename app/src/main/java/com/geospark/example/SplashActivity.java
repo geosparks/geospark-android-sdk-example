a3b5cca6fee107be18437aa92ec66ecb8c0c54ca
ekambaramj <ekambaram@storyboard.co>
2019-06-28 16:14:00 +0530

    SDK 2.0.0 updated

--- app/src/main/java/com/geospark/example/SplashActivity.java
+++ app/src/main/java/com/geospark/example/SplashActivity.java
@@ -5,17 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.iid.FirebaseInstanceId;
-
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_activity);
-        FirebaseApp.initializeApp(this);
-        FirebaseInstanceId.getInstance().getToken();
         Thread background = new Thread() {
             public void run() {
                 try {
